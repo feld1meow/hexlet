55595051499b5b3dbbabfd4323d87f5ca892e9aa
feld1meow <49448500+feld1meow@users.noreply.github.com>
2025-02-03 00:54:50 +0300

    Update scrap_structure.py

--- system/scrap_structure.py
+++ system/scrap_structure.py
@@ -1,24 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url = 'https://ru.hexlet.io/courses/python-setup-environment'
+url = "https://ru.hexlet.io/courses/python-overview-of-web-development"
 
 headers = {
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0)',
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0)",
 }
 
 
 session = requests.session()
 response = session.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, "html.parser")
 
 
 i = 1
 
-for element in soup.find_all(class_='row my-2'):
+for element in soup.find_all(class_="row my-2"):
     name = element.find(class_="text-decoration-none text-body me-2").text.strip()
     description = element.find(class_="mt-3 text-body-secondary").text.strip()
-    print(f'{i}. {name} - {description}')
+    print(f"{i}. {name} - {description}")
     i += 1
